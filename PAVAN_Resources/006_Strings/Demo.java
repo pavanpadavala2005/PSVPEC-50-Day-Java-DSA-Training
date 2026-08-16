a0e62fc73b62d7c83c2719b419cacc396e83b076
Pavan kumar <rokzpavan143@gmail.com>
2026-08-16 14:03:59 +0530

    Modifying PAVAN_Resourses

---
+++ PAVAN_Resources/006_Strings/Demo.java
@@ -0,0 +1,89 @@
+
+
+public class Demo {
+
+    public static void main(String[] args) {
+        // ! Aug 15 Stuff
+        // System.out.println(Aug15.reverseStringBrute("Hello"));
+        // System.out.println(Aug15.isPalindromeSentence(
+        // "Too hot to hoot"
+        // "Abc 012..## 10cbA"
+        //
+        // ));
+
+        // System.out.println(Aug15.isPalindromeNumber(-151));
+        // String s = new String("..geeks..for.geeks.");
+
+        // String[] arr = s.split(".");
+        // System.out.println(Arrays.toString(arr));
+
+        // System.out.println(Aug15.romanToIntegerBetter(
+        // "MCMXCIV"));
+
+        // System.out.println(
+        // Aug15.isIsomorphicOptimal(
+        // "egg",
+        // "add"
+        // "f11",
+        // "b23"
+        // "paper",
+        // "title"
+        // "bbbaaaba",
+        // "aaabbbba"
+
+        //
+        // ));
+        // System.out.println(Aug15.strStrOptimal(
+        // // "sadbutsad",
+        // // "sad"
+
+        // // "leetcode",
+        // // "leeto"
+
+        // "mississippi",
+        // "issip"
+
+        // ));
+
+        // System.out.println(Aug15.longestCommonPrefixOptimal(
+        // new String[] {
+        // "flower", "flow", "flight"
+        // // "dog", "racecar", " car"
+        // }
+        // //
+        // ));
+
+        // System.out.println(Aug15.largestOddNumber(
+        // "1234"
+        // //
+        // ));
+
+        // System.out.println(Aug15.rotateStringBrute(
+        // // "abcde", "cdeab"
+        // "abcde", "abced"
+        // //
+        // ));
+
+        // System.out.println(Aug15.isAnagramOptimal(
+        // "anagramz",
+        // "nagaramz"
+        // //
+        // ));
+
+        // System.out.println(Aug15.isAnagramOptimal(
+        // // "anagram", "nagaram"
+        // "rat", "car"
+        // //
+        // ));
+
+        // ! Aug 16 Stuff
+        // System.out.println(
+        // Aug16.reverseWordsBetter(
+        // "the sky is blue"
+        // " hello world "
+        // "a good example"
+        //
+        // ));
+
+    }
+}
